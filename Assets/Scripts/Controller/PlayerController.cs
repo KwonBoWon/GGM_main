395b39fff8bb1c8a5c1c57c4f6838103253e58d5
BoWon <bogod1207@naver.com>
2022-07-26 23:43:14 +0900

    Add TIme slider

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -11,9 +11,14 @@ public class PlayerController : MonoBehaviour
     int cnt = 0;
     BackGroundManager thebackGroundManager;
     public Slider MonsterHP;
+    public Slider TimeHP;
+
     public float maxHP = 100; //최대 체력
     public float curHP = 100; //현재 체력
 
+    private float maxTime = 50; //최대 시간
+    private float curTime = 50; //현재시간
+    private float addTime = 15; //시간추가
     private bool monsterLife = true;
 
     void Start()
@@ -21,34 +26,34 @@ public class PlayerController : MonoBehaviour
         Instantiate(goMonster[cnt], tfMonsterAppear.position, Quaternion.identity);
         MonsterHP = GameObject.Find("MonsterHP").GetComponent<Slider>();
         MonsterHP.value = (float) curHP / (float) maxHP;
+        TimeHP = GameObject.Find("Time").GetComponent<Slider>();
+        TimeHP.value =(float)curTime / (float)maxTime; //초기시간
+
+
         theTimingManager = FindObjectOfType<TimingManager>();
         thebackGroundManager = FindObjectOfType<BackGroundManager>();
     }
     void Update()
     {
-        //화살표 입
+        //화살표 입력
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             theTimingManager.CheckTiming(0);
-
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             theTimingManager.CheckTiming(1);
-            //Debug.Log("up");
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             theTimingManager.CheckTiming(2);
-            //Debug.Log("left");
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             theTimingManager.CheckTiming(3);
-            //Debug.Log("right");
         }
         HandleHP();
-
+        HandleTime();
     }
     public void HandleHP() {
         MonsterHP.value = (float) curHP / (float) maxHP;
@@ -56,7 +61,7 @@ public class PlayerController : MonoBehaviour
         if (MonsterHP.value == 0 && monsterLife == true) { // 적 죽으면
          	Destroy(GameObject.Find("monster" + cnt + "(Clone)"));
             cnt++;
-            Debug.Log(cnt);
+           // Debug.Log(cnt);
             MonsterHP.gameObject.SetActive(false);
 
             NoteManager.noteOn = false;
@@ -69,12 +74,30 @@ public class PlayerController : MonoBehaviour
             MakeMonster();
         }
     }
+    public void HandleTime()
+    {
+        TimeHP.value = (float) curTime / (float) maxTime;
+        if (TimeHP.value > 0.0f)
+        {
+            curTime -=Time.deltaTime; //시간 줄어듦
+        }
+        else //시간없을때(죽었을때)
+        {
+            
+        }
+    }
+
     public void MakeMonster() {
         curHP = 100;
         monsterLife = true;
         Instantiate(goMonster[cnt], tfMonsterAppear.position, Quaternion.identity);
         MonsterHP.gameObject.SetActive(true);
         //monsterhp=Instantiate(MonsterHP, tfMonsterAppear.position , Quaternion.identity);
+
+
+       
+        curTime += addTime; //시간추가
+        if(curTime>maxTime) curTime = maxTime;
     }
 
 }
