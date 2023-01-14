779b19ac04208ae5fd3dea8b6b16154231b3ec83
BoWon <bogod1207@naver.com>
2023-01-15 02:12:21 +0900

    Make Loading

--- Assets/Scripts/PlayGame.cs
+++ Assets/Scripts/PlayGame.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PlayGame : MonoBehaviour
 {
+    [SerializeField] GameObject MainCanvas;
+    [SerializeField] GameObject LoadingCanvas;
    public void Play() {
+        //여기다가 그냥 ui넣어서 셋액티브 가자고
+        MainCanvas.SetActive(false);
+
         SceneManager.LoadScene("Note");
    }
 }
